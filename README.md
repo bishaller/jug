452ee958a4a0a85e27a132339a0fd40bca476566
Bishal <bishal@lastdoorsolutions.com>
2021-02-12 00:41:26 +0545

    start: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# jug
-Is a foundation template to start your website development.
+# jug v1.0
+* Is a foundation template to start your website development.
+* Maintained & Updated by: **[Bishal Mishra](http://bishal.cc/)**
+
+
+## About
+Foobar is a Python library for dealing with word pluralization.
+
+## Installation
+* Uses [gulp](https://gulpjs.com/) with [npm](https://www.npmjs.com/)
+
+```bash
+$ npm install
+$ gulp
+```
+
+
+## Usage
+* Change the URL for your project at gulpfile.js
+* "gulp" or "npm start" 
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue/suggestions to discuss what could make this better.
+
+## License
+[Unlicense](https://unlicense.org/)
---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+/*
+
+Run these commands
+
+## if the project is already set up and running:
+npm install
+
+## if you check package.json and there aren't dependencies listed ( there should be no reason to do this )
+npm install gulp
+npm install gulp-concat --save-dev
+npm install gulp-plumber --save-dev
+npm install gulp-autoprefixer --save-dev
+npm install gulp-clean-css --save-dev
+npm install gulp-uglify --save-dev
+npm install gulp-rename --save-dev
+npm install gulp-notify --save-dev
+npm install gulp-sass --save-dev
+npm install gulp-sass-glob --save-dev
+npm install gulp-load-plugins --save-dev
+
+## always ( to compile/watch/etc )
+gulp
+*/
+
+// Config for theme
+var themePath  = './';
+var projectURL = 'http://jug.loc/';
+// Gulp Nodes
+var gulp        = require( 'gulp' ),
+    gulpPlugins = require( 'gulp-load-plugins' )();
+
+
+// Browserstack
+const browserSync = require('browser-sync').create();
+
+// Error Handling
+var onError = function( err ) {
+    console.log( 'An error occurred:', err.message );
+    this.emit( 'end' );
+};
+
+gulp.task('scss', function () {
+    const { concat, autoprefixer, cleanCss, notify, plumber, sass, sassGlob } = gulpPlugins;
+    // return sass(themePath + 'style.scss', { sourcemap: false })
+    return gulp.src(themePath + 'src/styles/style.scss', {sourcemap: true })
+        .on('error', sass.logError)
+        .pipe(plumber())
+        .pipe(sassGlob())
+        .pipe(sass())
+        .pipe(autoprefixer('last 4 version'))
+        .pipe(cleanCss())
+        .pipe(concat('dist/style.css'))
+        .pipe(gulp.dest(themePath))
+        .pipe(notify({ message: 'Styles Compiled' }));
+});
+
+gulp.task('scripts', function() {
+    const { concat, notify, plumber, rename, uglify } = gulpPlugins;
+    return gulp.src( [themePath + 'src/scripts/plugins/**/*.js', themePath + 'src/scripts/development/**/*.js'] )
+        .pipe(plumber())
+        .pipe(concat('dist/scripts.js'))
+        .pipe(gulp.dest(themePath))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest(themePath))
+        .pipe(notify({ message: 'Scripts Compiled' }));
+});
+
+// Watch task -- this runs on every save.
+gulp.task( 'watch', function() {
+    browserSync.init({
+        proxy: projectURL,
+        open: true,
+        injectChanges: true,
+    });
+
+    // Watch all .scss files
+    gulp.watch( themePath + '**/**/**/*.scss', gulp.series( 'scss' ) ).on('change',browserSync.reload);
+    gulp.watch( themePath + '**/**/*.html').on('change',browserSync.reload);
+    gulp.watch( themePath + '**/**/*.php').on('change',browserSync.reload);
+
+    // Watch js files
+    gulp.watch( themePath + 'src/scripts/development/**/*.js', gulp.series( 'scripts' ) ).on('change',browserSync.reload);
+});
+
+gulp.task('clearCache', function() {
+    // Still pass the files to clear cache for
+    gulp.src(themePath + '**/**/**/*.scss');
+    gulp.src(themePath + '**/**/*.html');
+    gulp.src(themePath + '**/**/*.php')
+      .pipe(cache.clear());
+  
+    // Or, just call this for everything
+    cache.clearAll();
+    on('change',browserSync.reload);
+  });
+
+// Default task -- runs scss and watch functions
+gulp.task( 'default', gulp.series('scripts', 'scss', 'watch'));
